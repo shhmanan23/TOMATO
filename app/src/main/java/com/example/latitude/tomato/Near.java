6b0112d96652db070929b086f978869c6bd35cc3
shhmanan23 <shhmanan23@gmail.com>
2018-04-30 22:24:11 +0530

    e1

--- app/src/main/java/com/example/latitude/tomato/Near.java
+++ app/src/main/java/com/example/latitude/tomato/Near.java
@@ -1,5 +1,6 @@
 package com.example.latitude.tomato;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Near extends Fragment {
 
@@ -16,12 +18,20 @@ public class Near extends Fragment {
 
     public View onCreateView(LayoutInflater layoutInflater  , ViewGroup con , Bundle SBI)
     {
-        View v = layoutInflater.inflate(R.layout.fav,con,false);
-        RecyclerView F_rv = (RecyclerView)v.findViewById(R.id.fav_recycle);
-        LinearLayoutManager lm1 = new LinearLayoutManager(getActivity().getApplicationContext());
-        F_rv.setLayoutManager(lm1);
-        CustomAdapter CA = new CustomAdapter(getActivity().getApplicationContext());
-        F_rv.setAdapter(CA);
+        View v = layoutInflater.inflate(R.layout.nearby,con,false);
+       // RecyclerView F_rv = (RecyclerView)v.findViewById(R.id.fav_recycle);
+        //LinearLayoutManager lm1 = new LinearLayoutManager(getActivity().getApplicationContext());
+        //F_rv.setLayoutManager(lm1);
+        //CustomAdapter CA = new CustomAdapter(getActivity().getApplicationContext());
+        //F_rv.setAdapter(CA);
+        Button viewnear = (Button)v.findViewById(R.id.NN);
+        viewnear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =  new Intent(getActivity(), GPS.class);
+                startActivity(i);
+            }
+        });
 
         return  v;
     }
